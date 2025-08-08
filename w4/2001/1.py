c5287dd25a4b13ccdc9259ce8b17d6b88cefa4bd
oriwacat <ksgg3911@gmail.com>
2025-08-08 17:56:13 +0900

    1

--- w4/2001/1.py
+++ w4/2001/1.py
@@ -1,27 +1,22 @@
+import sys
+sys.stdin = open("input.txt")
+
 T = int(input().strip())
 
 for test_case in range(1, T+1):
     N, M = map(int, input().split())
-    balloons = []
-    result = 0
-
+    matrix = [list(map(int,input().split())) for i in range(N)]
+    result = []
     for i in range(N):
-        balloons.append(list(map(int, input().split())))
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+        for j in range(N):
+            xx = 0
 
-    for i in range(N):
-        for j in range(M):
-            temp_sum = balloons[i][j]
-            for k in range(4):
-                for z in range(1, balloons[i][j] + 1):
-                    ni = i + (dx[k] * z)
-                    nj = j + (dy[k] * z)
-
-                    if 0 <= ni < N and 0 <= nj < M:
-                        temp_sum += balloons[ni][nj]
-
-            if temp_sum > result:
-                result = temp_sum
-
-    print(f'#{test_case} {result}')
+            for x in range(M):
+                for y in range(M):
+                    dx = i + x
+                    dy = j + y
+                    if 0 <= dx < N and 0 <= dy < N:
+                        xx += matrix[dx][dy]
+            
+            result.append(xx)
+    print(f'#{test_case} {max(result)}')
