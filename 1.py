82aa8addc0672e9d95463b2aaa94699728329090
oriwacat <ksgg3911@gmail.com>
2025-08-09 13:16:15 +0900

    1

--- 1.py
+++ 1.py
@@ -1,28 +1,4 @@
-T = int(input())
+array = [[i * 4 + j + 1 for j in range(4)] for i in range(5)]
+print(array)
 
-
-for tast_case in range(T):
-  N, M = map(int, input().split())
-  balloons = []
-  result = 0
-  
-  for i in range(N):
-    balloons.append(list(map(int, input().split())))
-  dx = [1, -1, 0, 0]
-  dy = [0, 0, 1, -1]
-
-  for i in range(N):
-    for j in range(M):
-      sum = 0
-      for k in range(4):
-        ni = i + dx[k]
-        nj = j + dy[k]
-
-        if 0 <= ni < N and 0 <= nj < M:
-          sum += balloons[ni][nj]
-          
-      if sum > result:
-        result = sum
-
-  print(f'#{tast_case} {result}')
-
+ai = [[i * 4 + j + 1 for j in range(4)] for i in range(5)]
