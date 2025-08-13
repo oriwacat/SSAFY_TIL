58ee20f759a3548ac1fcf0361c1303d37d6f1b98
oriwacat <ksgg3911@gmail.com>
2025-08-13 18:00:05 +0900

    1

--- w5/24973/24973.py
+++ w5/24973/24973.py
@@ -2,8 +2,38 @@ import sys
 sys.stdin = open("input.txt")
 
 T = int(input())
-
-for test_case in range(1, T+1):
+for tc in range(1, T + 1):
+    a = []
     N = int(input())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    for i in range()
+    map_list = [list(map(int, input().split())) for _ in range(N)]
+    for l in range(N):
+        for p in range(N):
+            a.append(map_list[l][p])
+    pos = max(a)
+    goal = min(a)
+    cnt = 0
+    ww = []
+    wc = []
+    result = []
+    box = []
+
+    x = (1, -1, 0, 0)
+    y = (0, 0, 1, -1)
+    while pos > goal:
+        for i in range(N):
+            for j in range(N):
+                if map_list[i][j] == pos:
+
+        for i in range(N):
+            for j in range(N):
+                if map_list[i][j] == pos:  # 20
+
+                    for k in range(4):
+                        dx = i + x[k]
+                        dy = j + y[k]
+                        if 0 <= dx < N and 0 <= dy < N:
+                            box.append(map_list[dx][dy])
+                    pos = min(box)  # 15
+                    cnt += 1
+        result.append(cnt)
+    print(f'#{tc} {max(result)}{ww}')
