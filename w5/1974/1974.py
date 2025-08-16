f63ca63be24a51a4e132b92d759f93dc70148847
oriwacat <ksgg3911@gmail.com>
2025-08-17 00:16:29 +0900

    1

--- w5/1974/1974.py
+++ w5/1974/1974.py
@@ -4,11 +4,30 @@ sys.stdin = open("input.txt")
 T = int(input())
 N = 9
 for t in range(1, T + 1):
+    result = 1
     matrix = [list(map(int, input().split())) for _ in range(N)]
-    for i in range(1, N+1):
+    for i in range(N):
         if sorted(matrix[i]) != [1,2,3,4,5,6,7,8,9]:
-            print(f'#{t} {0}')
+            result = 0
             break
-    for i in range(3):
-        for j in range(3):
-           
+    if result:
+        for i in range(3):
+            for j in range(3):
+                box = []
+                for x in range(3*i, 3*i+3):
+                    for y in range(3*j, 3*j+3):
+                        box.append(matrix[x][y])
+                if sorted(box) != [1,2,3,4,5,6,7,8,9]:
+                    result = 0
+                    break
+            if result == 0:
+                break
+    if result:
+        matrix_n = list(zip(*matrix))
+        for i in range(N):
+            if sorted(matrix_n[i]) != [1,2,3,4,5,6,7,8,9]:
+                result = 0
+                break
+
+    print(f'#{t} {result}') 
+
--- w5/24973/24973.py
+++ w5/24973/24973.py
@@ -11,29 +11,33 @@ for tc in range(1, T + 1):
             a.append(map_list[l][p])
     pos = max(a)
     goal = min(a)
-    cnt = 0
-    ww = []
-    wc = []
     result = []
-    box = []
-
+    spoint = []
+    xx = []
     x = (1, -1, 0, 0)
     y = (0, 0, 1, -1)
-    while pos > goal:
-        for i in range(N):
-            for j in range(N):
-                if map_list[i][j] == pos:
+    
+    for i in range(N):
+        for j in range(N):
+            if map_list[i][j] == pos:
+                spoint.append((i, j))
 
-        for i in range(N):
-            for j in range(N):
-                if map_list[i][j] == pos:  # 20
+    for i, j in spoint:
+        ii, jj = i, j
+        while True:
+            n_pos = 1000
+            box = []
+            cnt = 1
+            for k in range(4):
+                dx = ii + x[k]
+                dy = jj + y[k]
+                if 0 <= dx < N and 0 <= dy < N:
+                    box.append(map_list[dx][dy])
+                n_pos = min(box)  # 15
+                cnt += 1
+            
+            if n_pos == goal:
+                break
 
-                    for k in range(4):
-                        dx = i + x[k]
-                        dy = j + y[k]
-                        if 0 <= dx < N and 0 <= dy < N:
-                            box.append(map_list[dx][dy])
-                    pos = min(box)  # 15
-                    cnt += 1
         result.append(cnt)
-    print(f'#{tc} {max(result)}{ww}')
+    print(f'#{tc} {max(xx)}')
