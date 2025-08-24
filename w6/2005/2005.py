2effc118e198c09520cce8f9fe0a1bb9bb01e0d4
oriwacat <ksgg3911@gmail.com>
2025-08-24 15:35:06 +0900

    1

--- w6/2005/2005.py
+++ w6/2005/2005.py
@@ -4,14 +4,21 @@ sys.stdin = open("input.txt")
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
-    ne = []
-    pre = 1
-    for i in range(1, N+1):
+    stacker = []
+    print(f'#{tc}')
+    for i in range(1,N+1):
         result = []
+        stacker2 = []
+        stacker.insert(0,int(1))
         for j in range(i):
-            if ne[j-1] == True:
-                reuslt.append(ne[j-1]+ne[j-2])
+            x = stacker.pop()
+            if stacker2 and stacker :
+                result.append(stacker2[-1] + x)
+                stacker2.append(x)
             else:
-                result.append(pre)
-        print(result)
-        ne = result
+                stacker2.append(x)
+                result.append(x)
+
+        print(*result)
+        stacker = result
+
