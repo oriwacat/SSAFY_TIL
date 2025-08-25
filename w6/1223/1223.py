042e8bd49afe645087bc336a5175e7999f311c8d
oriwacat <ksgg3911@gmail.com>
2025-08-25 23:56:07 +0900

    1

--- w6/1223/1223.py
+++ w6/1223/1223.py
@@ -1,37 +1,6 @@
 import sys
 sys.stdin = open("input.txt")
 
-# for t in range(1, 11):
-#     N = int(input())
-#     C = list(input())
-#     box = []
-#     operator = []
-#     C.reverse()
-#     for i in range(N):
-#         if C[-1].isdigit():
-#             box.append(C.pop())
-#         else:
-#             if not operator:
-#                 operator.append(C.pop())
-#             elif C[-1] == '*':
-#                 if operator == '*':
-#                     box.append(C.pop())
-#                 else:
-#                     operator.append(C.pop())
-#             elif C[-1] == '+':
-#                 if operator:
-#                     box.append(operator.pop())
-#                     if operator:
-#                         box.append(operator.pop())
-#                 operator.append(C.pop())
-#     if operator:
-#         box.append(operator.pop())
-#         if operator:
-#             box.append(operator.pop())
-#
-#
-#     print(*box)
-
 for t in range(1, 11):
     N = int(input())
     C = list(input())
@@ -45,36 +14,76 @@ for t in range(1, 11):
             if not operator:
                 operator.append(C.pop())
             elif C[-1] == '*':
-                if operator == '*':
-                    C.pop()
-                    box.append(box.pop() * box.pop())
+                if operator[-1] == '*':
+                    box.append(C.pop())
                 else:
                     operator.append(C.pop())
             elif C[-1] == '+':
-                if operator[-1] == '*':
-                    operator.pop()
-                    box.append(box.pop() * box.pop())
-                elif operator[-1] == '+':
-                    operator.pop()
-                    box.append(box.pop() + box.pop())
-                if operator[-1] == '*':
-                    operator.pop()
-                    box.append(box.pop() * box.pop())
-                elif operator[-1] == '+':
-                    C.pop()
-                    box.append(box.pop() + box.pop())
-    if operator[-1] == '*':
-        operator.pop()
-        box.append(box.pop() * box.pop())
-    elif operator[-1] == '+':
-        operator.pop()
-        box.append(box.pop() + box.pop())
-    if operator[-1] == '*':
-        operator.pop()
-        box.append(box.pop() * box.pop())
-    elif operator[-1] == '+':
-        C.pop()
-        box.append(box.pop() + box.pop())
+                while operator:
+                    box.append(operator.pop())
+                operator.append(C.pop())
+    while operator:
+        box.append(operator.pop())
+
+    result = []
+    for i in box:
+        if i.isdigit():
+            result.append(int(i))
+        else:
+            a = result.pop()
+            b = result.pop()
+            if i == '+':
+                result.append(a + b)
+            elif i == '*':
+                result.append(a * b)
+
+    print(f'#{t} {result[0]}')
+
+    
+
+# for t in range(1, 11):
+#     N = int(input())
+#     C = list(input())
+#     box = []
+#     operator = []
+#     C.reverse()
+#     for i in range(N):
+#         if C[-1].isdigit():
+#             box.append(C.pop())
+#         else:
+#             if not operator:
+#                 operator.append(C.pop())
+#             elif C[-1] == '*':
+#                 if operator[-1] == '*':
+#                     C.pop()
+#                     box.append(box.pop() * box.pop())
+#                 else:
+#                     operator.append(C.pop())
+#             elif C[-1] == '+':
+#                 if operator[-1] == '*':
+#                     operator.pop()
+#                     box.append(box.pop() * box.pop())
+#                 elif operator[-1] == '+':
+#                     operator.pop()
+#                     box.append(box.pop() + box.pop())
+#                 if operator[-1] == '*':
+#                     operator.pop()
+#                     box.append(box.pop() * box.pop())
+#                 elif operator[-1] == '+':
+#                     C.pop()
+#                     box.append(box.pop() + box.pop())
+#     if operator[-1] == '*':
+#         operator.pop()
+#         box.append(box.pop() * box.pop())
+#     elif operator[-1] == '+':
+#         operator.pop()
+#         box.append(box.pop() + box.pop())
+#     if operator[-1] == '*':
+#         operator.pop()
+#         box.append(box.pop() * box.pop())
+#     elif operator[-1] == '+':
+#         C.pop()
+#         box.append(box.pop() + box.pop())
 
 
-    print(*box)
+#     print(*box)
