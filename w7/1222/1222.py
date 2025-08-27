fdb4ac6db5292ba7cabe0ddd4614a0346a05bc94
oriwacat <ksgg3911@gmail.com>
2025-08-27 16:12:27 +0900

    1

--- w7/1222/1222.py
+++ w7/1222/1222.py
@@ -4,23 +4,23 @@ from collections import deque
 
 T = 10
 for t in range(1, T+1):
-    N = int(input())
-    calcul = deque(input())
+    N = int(input().strip())
+    calcul = deque(input().strip())
     box = []
     operator = []
     for i in range(N):
-        if calcul[i].isdigit():
+        if calcul[0].isdigit():
             box.append(calcul.popleft())
         else:
             if operator:
                 a = box.pop()
                 b = box.pop()
-                operator.pop()
-                box.append(a + b)
+                calcul.popleft()
+                box.append(int(a) + int(b))
             else:
                 operator.append(calcul.popleft())
     a = box.pop()
     b = box.pop()
     operator.pop()
-    box.append(a + b)
+    box.append(int(a) + int(b))
     print(f'#{t} {box[0]}')
