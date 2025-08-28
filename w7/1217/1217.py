01cbf0a11bf56917706e4b5d310724451db4bba4
oriwacat <ksgg3911@gmail.com>
2025-08-28 17:54:55 +0900

    1

---
+++ w7/1217/1217.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+def exponentiation(N, M):
+    if M == 1:
+        return N
+    else:
+        return N * exponentiation(N, M-1)
+T = 10
+for t in range(1, T+1):
+    h = int(input())
+    N, M = map(int, input().split())
+    print(f'#{h} {exponentiation(N, M)}')
