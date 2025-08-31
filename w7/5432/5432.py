b56715605a0f7a3ef8a60b784ab1e1c0a94f6440
oriwacat <ksgg3911@gmail.com>
2025-08-31 23:30:59 +0900

    1

--- w7/5432/5432.py
+++ w7/5432/5432.py
@@ -7,10 +7,18 @@ for t in range(1, T+1):
     result = 0
     pip = []
     box = list(input())
-    for i in range(len(box)):
-        if box[] == ')':
+    bl = len(box)
+    for i in reversed(range(bl)):
+        if box[i] == '(':
+            if box[i+1] == ')':
+                pip.pop()
+                result += len(pip)
+                continue
             pip.pop()
-            result += len(pip)
-        pip.append(i)
+            result += 1
+            continue
+        pip.append(box[i])
+
+    print(f'#{t} {result}')
 
     # 괄호 열려있는 동안 레이저 지나가면 괄호 개수 를 result에 추가 하기
