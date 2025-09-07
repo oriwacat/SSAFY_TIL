59d4140c1626b35841fa1dd2fd1703033edfc4b5
oriwacat <ksgg3911@gmail.com>
2025-09-07 22:35:32 +0900

    1

--- w8/완탐.py
+++ w8/완탐.py
@@ -1,20 +1,94 @@
-def comb(arr, n):
-    result = []  # 조합을 저장할 리스트
+# def comb(arr, n):
+#     result = []  # 조합을 저장할 리스트
+#
+#     if n == 1:
+#         return [[i] for i in arr]
+#
+#     for i in range(len(arr)):
+#         elem = arr[i]
+#
+#         for rest in comb(arr[i + 1:], n - 1):  # 조합
+#             # for rest in comb(arr[:i] + arr[i+1:], n - 1):  # 순열
+#             # for rest in comb(arr, n - 1):  # 중복순열
+#             # for rest in comb(arr[i:], n - 1):  # 중복조합
+#             result.append([elem] + rest)
+#
+#     return result
+#
+# for i in range(1,5):
+#     print(comb([1, 2, 3, 4], i))
+#     1
 
-    if n == 1:
-        return [[i] for i in arr]
 
-    for i in range(len(arr)):
-        elem = arr[i]
+def subsets(nums, index=0, current=None):
+    if current is None:
+        current = []
 
-        for rest in comb(arr[i + 1:], n - 1):  # 조합
-            # for rest in comb(arr[:i] + arr[i+1:], n - 1):  # 순열
-            # for rest in comb(arr, n - 1):  # 중복순열
-            # for rest in comb(arr[i:], n - 1):  # 중복조합
-            result.append([elem] + rest)
+    # 종료 조건: 끝까지 탐색했으면 출력
+    if index == len(nums):
+        print(current)
+        return
 
-    return result
+    # 1. 현재 원소를 포함하지 않는 경우
+    subsets(nums, index + 1, current)
 
-for i in range(1,5):
-    print(comb([1, 2, 3, 4], i))
-    1
+    # 2. 현재 원소를 포함하는 경우
+    subsets(nums, index + 1, current + [nums[index]])
+
+
+# 실행
+subsets([1, 2, 3])
+
+# -----------------------------------------------------------
+
+# def comb(arr, n):
+#     result = []
+#     if n == 1:
+#         return [[i] for i in arr]
+#
+#     for i in range(len(arr)):
+#         elem = arr[i]
+#         for rest in comb(arr[i + 1:], n - 1):  # 조합
+#             result.append([elem] + rest)
+#
+#     return result
+# print(comb([1, 2, 3, 4], 2))
+
+
+# def perm(arr, n):
+#     result = []
+#     if n == 1:
+#         return [[i] for i in arr]
+#
+#     for i in range(len(arr)):
+#         elem = arr[i]
+#         for rest in perm(arr[:i] + arr[i + 1:], n - 1):  # 순열
+#             result.append([elem] + rest)
+#     return result
+# print(perm([1, 2, 3, 4], 2))
+#
+
+
+# def h_perm(arr, n):
+#     result = []
+#     if n == 1:
+#         return [[i] for i in arr]
+#
+#     for elem in arr:  # 인덱스가 아닌 요소를 직접 순회
+#         for rest in h_perm(arr, n - 1):  # 중복순열
+#             result.append([elem] + rest)
+#     return result
+# print(h_perm([1, 2, 3, 4], 2))
+#
+# def h_comb(arr, n):
+#     result = []
+#     if n == 1:
+#         return [[i] for i in arr]
+#
+#     for i in range(len(arr)):
+#         elem = arr[i]
+#         for rest in h_comb(arr[i:], n - 1):  # 중복조합
+#             result.append([elem] + rest)
+#     return result
+# print(h_comb([1, 2, 3, 4], 2))
+#
