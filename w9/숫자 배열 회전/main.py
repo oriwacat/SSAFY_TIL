52f96cab2aa49cb17d983b6aeea0ed0556ba0d27
oriwacat <ksgg3911@gmail.com>
2025-09-09 00:03:33 +0900

    1

--- w9/숫자 배열 회전/main.py
+++ w9/숫자 배열 회전/main.py
@@ -10,5 +10,13 @@ for t in range(1,T+1):
     matrix = [list(map(int,input().split())) for _ in range(N)]
     result = []
     for i in range(3):
-        result.append(role(matrix))
-    print(result)
+        matrix = role(matrix)
+        result.append(matrix)
+
+    print(f'#{t}')
+    for i in range(N):
+        line = []
+        for j in range(3):
+            line.append(''.join(map(str, result[j][i])))
+
+        print(' '.join(line))
