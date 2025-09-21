6c8d55dafa99a6a0f0e04738ad63d6defe95e0fd
oriwacat <ksgg3911@gmail.com>
2025-09-21 23:29:56 +0900

    1

--- 연습장.py
+++ 연습장.py
@@ -1,21 +1,19 @@
 n, m = map(int, input().split())
 numbers = [int(input()) for _ in range(n)]
-trigger = True
 while True:
-
-    for i in range(len(numbers)):
-        trigger = True
-        for j in range(1,m):
-            if i+j < n and numbers[i] != numbers[i+j]:
-                trigger = False
-        if trigger:
-            for _ in range(m):
-                numbers.pop(i)
-
+    trigger = False
+    for i in reversed(range(len(numbers))):
+        j = i + 1
+        while j < len(numbers) and numbers[j] == numbers[i]:
+            j += 1
+        
+        block_len = j - i
+        
+        if block_len >= m:
+            del numbers[i:j]
+            trigger = True
             break
-
-    if len(numbers) == 0:
-        break
+    
     if not trigger:
         break
 
@@ -23,5 +21,6 @@ while True:
 if len(numbers) == 0:
     print(0)
 else:
+    print(len(numbers))
     for i in numbers:
         print(i)
