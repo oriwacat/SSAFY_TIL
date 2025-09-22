f49905c889e97ee621588a1eb802a8df10ea2b6f
김종환 <ksgg3911@gmail.com>
2025-09-22 17:59:13 +0900

    1

--- 연습장.py
+++ 연습장.py
@@ -1,26 +1,20 @@
-n, m = map(int, input().split())
-numbers = [int(input()) for _ in range(n)]
+n, m, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+k -= 1
+trigger = True
 while True:
-    trigger = False
-    for i in reversed(range(len(numbers))):
-        j = i + 1
-        while j < len(numbers) and numbers[j] == numbers[i]:
-            j += 1
-        
-        block_len = j - i
-        
-        if block_len >= m:
-            del numbers[i:j]
-            trigger = True
-            break
-    
-    if not trigger:
+
+    for i in range(n):
+        if all(x == 0 for x in grid[i][k:m]):
+            for j in range(k,m):
+                grid[i][j] = 1
+                if 0 <= grid[i-1][j] < n:
+                    grid[i-1][j] = 0
+                continue
+
+
+
+    if trigger:
         break
 
-
-if len(numbers) == 0:
-    print(0)
-else:
-    print(len(numbers))
-    for i in numbers:
-        print(i)
+print(grid)
