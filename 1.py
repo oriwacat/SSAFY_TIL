a974912039f781a94dc199be200bd2a9cfef1ebf
김종환 <ksgg3911@gmail.com>
2025-09-23 18:00:25 +0900

    1

--- 1.py
+++ 1.py
@@ -1,36 +1,26 @@
-import sys
-sys.stdin = open("w5/sample_input.txt", "r")
+import pprint
 
-def check_brackets(line):
-    """주어진 문자열의 괄호 짝이 맞는지 검사하는 함수"""
-    stack = []
-    # 닫는 괄호를 key, 여는 괄호를 value로 하는 딕셔너리
-    bracket_map = {')': '(', '}': '{'}
+n =10
+list1 = [[i]*n for i in range(n)]
 
-    for char in line:
-        # 여는 괄호는 스택에 추가
-        if char in '({':
-            stack.append(char)
-        # 닫는 괄호일 경우
-        elif char in bracket_map:
-            # 스택이 비어있으면 짝이 안 맞음 (닫는 괄호가 먼저 나옴)
-            if not stack:
-                return 0
-            # 스택의 마지막 괄호를 꺼냄
-            last_open_bracket = stack.pop()
-            # 꺼낸 괄호가 짝이 맞지 않으면 실패
-            if last_open_bracket != bracket_map[char]:
-                return 0
 
-    # 문자열 순회가 끝난 후 스택에 여는 괄호가 남아있으면 실패
-    if stack:
-        return 0
-    
-    # 모든 조건을 통과하면 성공
-    return 1
+pprint.pprint(list1)
+print('-'*100)
 
-T = int(input())
-for test_case in range(1, T + 1):
-    code_line = input()
-    result = check_brackets(code_line)
-    print(f"#{test_case} {result}")
+#전치행렬
+list2 = list(map(list, zip(*list1)))
+print('전치행렬')
+pprint.pprint(list2)
+print('-'*100)
+
+#90도 시계 방향 회전
+list3 = list(zip(*list2[::-1]))
+print('90도')
+pprint.pprint(list3)
+print('-'*100)
+
+#90도 반시계 방향 회전
+list4 = list(zip(*list3))[::-1]
+print('-90도')
+pprint.pprint(list4)
+print('-'*100)
---
+++ w11/창용 마을 무리의 개수/main.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin = open('s_input.txt')
+
+def find_set(x):
+    if x != p[x]:
+        p[x] = find_set(p[x])
+    return p[x]
+
+def union(x, y):
+    px = find_set(x)
+    py = find_set(y)
+
+    if px != py:
+        if rank[px] > rank[py]:
+            p[py] = px
+        elif rank[px] < rank[py]:
+            p[px] = py
+        else:
+            p[py] = px
+            rank[px] += 1
+
+T = int(input())
+
+for t in range(1, T+1):
+    n, m = map(int,input().split())
+    p = list(range(n+1))
+    rank = [0] * (n+1)
+
+    for i in range(m):
+        x, y = map(int,input().split())
+        union(x, y)
+
+    result = set()
+    for v in range(1,n+1):
+       result.add(find_set(v))
+    print(f'#{t} {len(result)}')
